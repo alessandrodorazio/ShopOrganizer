59936be7b6a238cd68e5c2e34cc5ee5f3ee16568
unknown <riccardo.iovenitti@gmail.com>
2020-05-04 18:56:10 +0200

    lista prodotti

--- mobile_app/src/app/tab1/tab1.page.ts
+++ mobile_app/src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+
+import { Product } from '../model/ProductModel';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +9,52 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  @ViewChild(IonInfiniteScroll, {static: false}) infiniteScroll: IonInfiniteScroll;
+  products: Product[] = [];
+  id = 0;
+  prodNameA = ['Tonno', 'Olio', 'Pane', 'Lenticchie', 'Prosciutto', 'Mozzarella', 'Fettine', 'Olive', 'Detersivo', 'Minestrone'];
+  prodNameB = ['Oliva', 'Integrale', 'Bianco', 'Fresca', 'Appassita', 'Cotto', 'Crudo', 'Al dente', 'Scelte', 'al Naturale',
+               'Sott\'olio', 'Sott\'aceto', 'Liquido', 'Solido'];
+  pezzatura = ['3x100g', '1L', '2L', '100g', '200g', '1pz', '500ml', '1Kg'];
 
-  constructor() {}
+  constructor() {
+      for (let x = 0; x < 50; x++) {
+        this.products.push(new Product(this.id++, this.getProductName() + ' (' + this.id + ')', this.getPezzatura(),
+                           this.random(0, 5), '/assets/product/' + this.random(0, 5) + '.jpg'));
+      }
+  }
 
+  loadData(event: any) {
+    setTimeout(() => {
+      for (let x = 0; x < this.random(10, 20); x++) {
+        this.products.push(new Product(this.id++, this.getProductName() + ' (' + this.id + ')', this.getPezzatura(),
+                                       this.random(0, 5), '/assets/product/' + this.random(0, 5) + '.jpg'));
+      }
+
+      console.log('Done: ' + this.id);
+      event.target.complete();
+
+      if (this.products.length >= 100) {
+        event.target.disabled = true;
+      }
+    }, 500);
+  }
+
+  toggleInfiniteScroll() {
+    this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
+  }
+
+  private random(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  private getProductName() {
+      return this.prodNameA[this.random(0, this.prodNameA.length)] + ' ' + this.prodNameB[this.random(0, this.prodNameB.length)]
+  }
+
+  private getPezzatura() {
+      return this.pezzatura[this.random(0, this.pezzatura.length)];
+  }
 }
