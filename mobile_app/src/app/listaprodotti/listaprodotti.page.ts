af8687cf7b1348e7795794d262ed8ad13cd8a271
Alessandro D'Orazio <me@alessandrodorazio.it>
2020-05-27 13:21:56 +0200

    restyling listaprodotti

--- mobile_app/src/app/listaprodotti/listaprodotti.page.ts
+++ mobile_app/src/app/listaprodotti/listaprodotti.page.ts
@@ -53,6 +53,7 @@ export class ListaProdottiPage implements OnInit {
   private aggiornaElenco() {
     if (this.tabellaProdotti.length === 0) { return; }
 
+    
     // Elementi per pagina
     const EL_PER_PAGINA = 20;
 
