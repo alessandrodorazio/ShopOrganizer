0964ba8788b4d616026e5472318cf444e7ba239a
Alessandro D'Orazio <me@alessandrodorazio.it>
2020-05-31 19:59:19 +0200

    sync localstorage on init and save list

--- mobile_app/src/app/app.component.ts
+++ mobile_app/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AppStateService } from './service/appstate.service';
+import { Utente } from './model/utente';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +13,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent {
   constructor(
+    private appState: AppStateService, 
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
@@ -21,6 +24,19 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
+
+      let infoUtente = new Utente();
+      if(localStorage.getItem('user') !== null){
+        let user = JSON.parse(localStorage.getItem('user'));
+        infoUtente.email = user.email;
+        infoUtente.nome = user.nome;
+        infoUtente.raggioKm = user.raggio_km;
+        infoUtente.maxRisultati = user.max_negozi;
+        infoUtente.ordinamento = user.preferenza_filtro==1?'PREZZO':'DISTANZA';
+        infoUtente.listaSalvata = user.lista[0].prodotti;
+      }
+      this.appState.add(Utente.UTENTE_KEY, infoUtente);
+
       this.splashScreen.hide();
     });
   }
--- mobile_app/src/app/listaprodotti/listaprodotti.page.ts
+++ mobile_app/src/app/listaprodotti/listaprodotti.page.ts
@@ -126,6 +126,43 @@ export class ListaProdottiPage implements OnInit {
       infoUtente.listaSalvata = daSalvare;
       this.appState.add(Utente.UTENTE_KEY, infoUtente);
 
+      let token = localStorage.getItem('token');
+      let user = JSON.parse(localStorage.getItem('user'));
+
+
+      let body = {
+        "user": {
+          "lista": {
+            "prodotti": infoUtente.listaSalvata.map(e => e.id)
+          }
+        }
+      };
+  
+  
+      async function postData(url = '', data = {}) {
+        const response = await fetch(url, {
+          method: 'PUT', // *GET, POST, PUT, DELETE, etc.
+          mode: 'cors', // no-cors, *cors, same-origin
+          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+          credentials: 'same-origin', // include, *same-origin, omit
+          headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          redirect: 'follow', // manual, *follow, error
+          referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+          body: JSON.stringify(data)
+        });
+        return response.json(); // parses JSON response into native JavaScript objects
+      }
+      console.log('https://shoporganizer.herokuapp.com/public/api/users/' + user.id + '?token=' + token);
+      postData('https://shoporganizer.herokuapp.com/public/api/users/' + user.id + '?token=' + token, body)
+        .then(data => {
+          localStorage.removeItem('user');
+          localStorage.setItem('user', JSON.stringify(data.user));
+          //TODO show alert
+        }).catch(err => console.error(err));
+
       this.router.navigate(['/tabs/listasalvata']);
     }
   }
