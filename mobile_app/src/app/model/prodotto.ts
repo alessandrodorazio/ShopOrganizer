f6d6401f3b08f11accccc96741c66efb0f6fb2b1
unknown <riccardo.iovenitti@gmail.com>
2020-06-18 19:34:01 +0200

    * renamed filename (all lowercase)

---
+++ mobile_app/src/app/model/prodotto.ts
@@ -0,0 +1,23 @@
+// Model prodotto
+
+export class Prodotto {
+    id: number;
+    codiceean: string;
+    marca: string;
+    nome: string;
+    immagine: string;
+    pezzatura: string;
+    createdat: Date;
+    updatedat: Date;
+
+    constructor() {}
+}
+
+export class ProdottoPrezzato extends Prodotto {
+    prezzo: number;
+    quantita: number;
+
+    constructor() {
+        super();
+    }
+}
